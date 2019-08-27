78ae1f7d2933096069b380dfa3f0312a51485a3d
Mercy Nekesa <kesamercy@gmail.com>
2019-08-27 12:03:36 -0400

    initial commit for hw0

---
+++ src/ub/cse/algo/Solution.java
@@ -0,0 +1,35 @@
+package ub.cse.algo;
+
+import java.util.Arrays;
+import java.util.Vector;
+import java.util.ArrayList;
+
+/**
+ * For use in CSE 331
+ * Template for Homework 3
+ */
+public class Solution {
+    
+    private ArrayList<Integer> in_vector;
+    
+    /**
+     * The constructor simply sets up the necessary data structures.
+     * The grader for the homework will first call this class and pass the necessary variables.
+     * There is no need to edit this constructor.
+     *
+     * @param in_vector the vector used to multiply against the matrix
+     */
+    public Solution(ArrayList<Integer> in_vector) {
+        this.in_vector = in_vector;
+    }
+    
+    /**
+     * This method must be filled in by you. You may add other methods and subclasses as you see fit,
+     * but they must remain within the Solution class.
+     * @return Your resulting vector.
+     */
+    public ArrayList<Integer> outputVector() {
+
+        return new ArrayList<Integer>();
+    }
+}
